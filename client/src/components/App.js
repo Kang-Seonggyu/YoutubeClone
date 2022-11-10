c9d20f14e89b44b7d993723b3060f42971dac861
Kang-Seonggyu <jokekim609@gmail.com>
2022-11-10 15:22:15 +0900

    비디오 디테일 페이지 만들기

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,6 +8,7 @@ import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import VideoUploadPage from './views/VideoUploadPage/VideoUploadPage';
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer";
+import VideoDetailPage from './views/Video/VideoDetailPage';
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -23,6 +24,7 @@ function App() {
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
           <Route exact path="/video/upload" component={Auth(VideoUploadPage, true)} />
+          <Route exact path="/video/:videoId" component={Auth(VideoDetailPage, null)} />
         </Switch>
       </div>
       <Footer />
---
+++ client/src/components/views/Video/VideoDetailPage.js
@@ -0,0 +1,58 @@
+import { Avatar, Col, List, Row } from "antd";
+import Axios from "axios";
+import React, { useState, useEffect } from "react";
+
+function VideoDetailPage(props) {
+
+    const videoId = props.match.params.videoId
+    const variable = { videoId : videoId }
+
+    const [VideoDetail, setVideoDetail] = useState([])
+
+    useEffect(() => {
+        Axios.post('/api/video/getVideoDetail', variable)
+            .then(response => {
+                if(response.data.success) {
+                    // console.log(response.data.videoDetail)
+                    setVideoDetail(response.data.videoDetail)
+                } else {
+                    alert('비디오 정보 가져오기를 실패했습니다.')
+                }
+            })
+    }, [])
+    
+    if (VideoDetail.writer) {
+        return (
+
+        
+            <Row gutter={[16, 16]}>
+                <Col lg={18} xs={24}>
+                    <div style={{ width : '100%', padding : '3rem 4rem'}}>
+                        <video style={{ width : '100%'}} src={`http://localhost:5000/${VideoDetail.filePath}`} contorls />
+                        
+                        <List.Item
+                            actions
+                        >
+                            <List.Item.Meta
+                                avatar={<Avatar src={VideoDetail.writer.image} />}
+                                title={VideoDetail.writer.name}
+                                description={VideoDetail.description}
+                            />
+                        </List.Item>
+                        comment
+    
+                    </div>
+                </Col>
+                <Col lg={6} xs={24}>
+                    Side Videos
+                </Col>
+    
+            </Row>
+        )
+    } else {
+        return ( <div></div>)
+    }
+   
+}
+
+export default VideoDetailPage;
